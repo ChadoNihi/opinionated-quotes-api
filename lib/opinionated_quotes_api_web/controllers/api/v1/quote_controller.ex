0a2970c2a145d74880e619d994bc84136e3eac11
ChadoNihi <chadoishere@gmail.com>
2018-03-08 22:37:05 +0200

    redirect on empty params

--- lib/opinionated_quotes_api_web/controllers/api/v1/quote_controller.ex
+++ lib/opinionated_quotes_api_web/controllers/api/v1/quote_controller.ex
@@ -1,15 +1,15 @@
 defmodule OpinionatedQuotesApiWeb.API.V1.QuoteController do
   use OpinionatedQuotesApiWeb, :controller
   alias OpinionatedQuotesApi.QuoteAPI.QuoteAPI
+  import OpinionatedQuotesApiWeb.Router.Helpers, only: [api_v1_quote_path: 3]
 
-  @n_all_re ~r/^all$/i
-  @true_vals_for_rand MapSet.new([nil, "", "true", "1", "t"])
+  @n_max_re ~r/^max|all$/i
+  @true_vals_for_rand MapSet.new([nil, "", "true", "1", "t", "true", "1", "", nil])
 
-  def get_quotes(conn, params) do
-    rand = if MapSet.member?(@true_vals_for_rand, params["rand"]), do: true, else: false
+  def get_quotes(conn, %{"rand" => raw_rand, "n" => raw_n} = params) do
+    rand = if MapSet.member?(@true_vals_for_rand, raw_rand), do: true, else: false
 
-    raw_n = Map.get(params, "n", "1")
-    n = if Regex.match?(@n_all_re, raw_n), do: false, else: (case Integer.parse(raw_n) do
+    n = if Regex.match?(@n_max_re, raw_n), do: false, else: (case Integer.parse(raw_n) do
       {num, _} ->
         (if num > 0, do: num, else: 0)
       :error ->
@@ -43,5 +43,10 @@ defmodule OpinionatedQuotesApiWeb.API.V1.QuoteController do
     |> String.downcase()
 
     json(conn, QuoteAPI.list_quotes(rand: rand, n: n, offset: offset, author: author, lang: lang))
+  end
+  def get_quotes(conn, params) do
+    redirect(conn, to: api_v1_quote_path(
+      conn, :get_quotes, Keyword.merge([rand: "t", n: 1], Keyword.new(params))
+    ))
   end
 end
